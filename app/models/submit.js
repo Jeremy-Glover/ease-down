bd8f7bb4e1eb38f8b1ae45faa27744b951ffd668
Jeremy Glover <jeremyglover33@gmail.com>
2016-01-05 18:20:05 -0600

    User can select region on map

---
+++ app/models/submit.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  email: DS.attr('string'),
+  phone: DS.attr('number'),
+  dates: DS.attr('number'),
+  url: DS.attr('string'),
+  location: DS.attr('string'),
+  description: DS.attr('string'),
+});
