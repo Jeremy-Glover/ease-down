786ba5930b46730c171edf4877a3ab34519eedd4
Jeremy Glover <jeremyglover33@gmail.com>
2016-01-07 16:39:22 -0600

    Added individual svg regions to region page

--- app/guide/submit/route.js
+++ app/guide/submit/route.js
@@ -4,9 +4,8 @@ export default Ember.Route.extend({
   actions: {
     submitEvent(formValues) {
       // Do stuff to create event model, set properties, and save
-      let newEvent = this.modelFor('event');
-      newEvent.setProperties(formValues);
-      newEvent.save().then(() => {
+      let event = this.store.createRecord('event', formValues);
+      event.save().then(() => {
         this.transitionTo('guide');
       });
     },
